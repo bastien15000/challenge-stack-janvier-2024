b6513afd6eb587b8ffcfbf32fd480f17ad8d479f
Bastien TAKIS <bastientakis@gmail.com>
2024-01-09 10:55:06 +0100

    Integrate layout

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Image from 'next/image'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <Image
+            src="/logo.png"
+            alt="Vercel Logo"
+            width={318}
+            height={80}
+          />
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
