836911f4a17b585b9adbcca294478374c283a9e3
Alex M <alexandremonierlycee@gmail.com>
2024-01-10 16:36:23 +0100

    Modification select client

--- front/src/components/Input/Input.tsx
+++ front/src/components/Input/Input.tsx
@@ -3,11 +3,12 @@ import PinkBorderTextField from "./Input.styles"
 
 export const Input: React.FC<InputProps> = (props) => {
   return (
-    <PinkBorderTextField label={props.label} type={props.type}/>
+    <PinkBorderTextField id={props.id} label={props.label} type={props.type}/>
   )
 }
 
 export interface InputProps {
   label: string,
   type: string,
+  id: string,
 }
--- front/src/components/Input/SelectBox.tsx
+++ front/src/components/Input/SelectBox.tsx
@@ -1,17 +1,43 @@
 import React from "react";
 import Select from 'react-select'
-
+import { Input } from "@/components/Input/Input"
 
 export const SelectBox: React.FC<SelectBoxProps> = (props) => {
+  function handleAddrTypeChange(e: any, id: any) {
+    let textLabel = "";
+    switch (e.value) {
+      case "Casse":
+        textLabel = "Nombre de lot cassé ?";
+        break
+      case "Incomplet":
+        console.log("incomplet")
+        textLabel = "Combien vous manque t'il de lot de vaccin ?";
+        break
+      case "Retard":
+        console.log("retard")
+        textLabel = "combien de minutes de retard ?";
+        break
+    }
+    if(textLabel != "") {
+      let Quantite = document.getElementById(id + "-label");
+      Quantite.textContent = textLabel;
+    }
+    
+  }
+
   return (
-    <div className="">
+    <div>
     <label className="MuiFormLabel-root MuiFormLabel-colorPrimary css-u4tvz2-MuiFormLabel-root">{props.label} :</label>
-    <Select options={props.options}/>
+    <Select onChange={e => handleAddrTypeChange(e, props.id)} id={props.id} options={props.options}/>
+    <br/>
+    <Input id={props.id} label="Quantité" type="number"/>
     </div>
   )
+
 }
 
 export interface SelectBoxProps {
   label: string,
+  id: string,
   options: Array<Object>,
 }
