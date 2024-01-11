13dd10fab9e63fd33ee9b873a252e0d9c81cd1ce
Bastien TAKIS <bastientakis@gmail.com>
2024-01-11 17:20:53 +0100

    merge

--- front/src/app/delivery/page.tsx
+++ front/src/app/delivery/page.tsx
@@ -1,35 +1,36 @@
 "use client"
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../../app/layout"
 import Customer from "@/components/Delivery/Customer";
 import "../../styles/client.css"
 import { Api } from "@/services/api"
 
 function Delivery() {
-  Api.ajax("orders", "GET")
-    .then(data => {
-      console.log(data.content)
-    })
+  const [orders, setOrders] = useState([]) // Initialisation de l'état
+
+  useEffect(() => {
+    Api.ajax("orders", "GET")
+      .then(data => {
+        setOrders(data.content) // Mise à jour de l'état avec les nouvelles données
+      })
+  }, [])
+
   return (
     <Layout>
       <div className="client-list">
-        <Customer data={{
-          img_src: "/Theraputics.png",
-          name: "McDonalds",
-          num: 432990,
-          state: "green",
-          address: "2 rue du nord",
-          date: "6/3/22"
-        }} />
-        <Customer data={{
-          img_src: "/Theraputics.png",
-          name: "McDonalds",
-          num: 432990,
-          state: "orange",
-          address: "2 rue du nord",
-          date: "6/3/22"
-        }} />
+        {
+          orders?.map(function (order) {
+            return <Customer data={{
+              img_src: "/Theraputics.png",
+              name: "McDonalds",
+              num: order?.id,
+              state: "green",
+              address: "2 rue du nord",
+              date: order.expected_time
+            }} />
+          })
+        }
       </div>
     </Layout>
   )
