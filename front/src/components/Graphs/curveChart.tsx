0cb29659df487c90be1306327003d040eeda2298
Alex M <alexandremonierlycee@gmail.com>
2024-01-11 21:20:25 +0100

    test

--- front/src/components/Graphs/curveChart.tsx
+++ front/src/components/Graphs/curveChart.tsx
@@ -10,38 +10,41 @@ const CurveChart: React.FC<ChartData> = (props) => {
         Api.ajax("kpi/serviceRate", "GET")
       .then(data => {
         setInfos(data.perDays) // Mise à jour de l'état avec les nouvelles données
-        const config = {
-            type: 'line',
-            data: {
-                labels: props.labels,
-                datasets: [{
-                    label: '',
-                    data: info,
-                    borderColor: props.borderColor,
-                    backgroundColor: props.backgroundColor,
-                    tension: props.tension
-                }]
-            },
-            options: {
-                plugins: {
-                    legend: {
-                        display: false
-                    }
+        
+      })
+      const config = {
+        type: 'line',
+        data: {
+            labels: props.labels,
+            datasets: [{
+                label: '',
+                data: info,
+                borderColor: props.borderColor,
+                backgroundColor: props.backgroundColor,
+                tension: props.tension
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false
                 }
             }
-        };
+        }
+    };
 
-        var myChart = new Chart(
-          chartRef.current,
-          config
-        );
-        return () => {
-            if (myChart) {
-                myChart.destroy();
-            }
-        };
-      })
+    var myChart = new Chart(
+      chartRef.current,
+      config
+    );
+    return () => {
+        if (myChart) {
+            myChart.destroy();
+        }
+    };
     }, [])
+
+    
 
     return (
       <canvas ref={chartRef} id="myChart"></canvas>
