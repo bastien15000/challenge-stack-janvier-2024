a20a7419d778a0a3ac1ae266c5887e2836318708
Bastien TAKIS <bastientakis@gmail.com>
2024-01-11 16:50:28 +0100

    [EDIT] - API service

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,8 +1,36 @@
+import { redirect } from 'next/navigation'
+
 export const Api = {
+    login: function (auth: any) {
+        return fetch("http://localhost:8000/api/login", {
+            method: "POST",
+            body: JSON.stringify(auth),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.roles.includes("ROLE_MANAGER")) {
+                    // redirect("/")
+                    window.location.href = "/"
+                } else if (data.roles.includes("ROLE_DELIVERYMAN")) {
+                    // redirect("/delivery")
+                    window.location.href = "/delivery"
+                } else if (data.roles.includes("ROLE_CLIENT")) {
+                    // redirect("/client")
+                    window.location.href = "/client"
+                }
+
+            })
+    },
     ajax: function (route: any, action: any) {
-        return fetch("localhost:8000/api/" + route, {
+        return fetch("http://localhost:8000/api/" + route, {
             method: action,
-            headers: { 'Content-Type': 'application/json' }
-        }).then(response => response.json()).then(data => console.log(data))
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem("token")
+            }
+        }).then(response => response.json())
     }
 }
