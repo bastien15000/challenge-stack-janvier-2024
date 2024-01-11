d5ae88899a8af0df6df2956a88b13f310d891191
Pellegrin69 <pellegrinpierre@hotmail.fr>
2024-01-11 22:44:39 +0100

    Debugging rating form

--- front/src/components/Dialogs/SatisfiedCustomerDialog.tsx
+++ front/src/components/Dialogs/SatisfiedCustomerDialog.tsx
@@ -1,97 +1,95 @@
 "use client"
 
-import * as React from 'react'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import CloseIcon from '@mui/icons-material/Close'
 import Typography from '@mui/material/Typography'
-import { SelectBox } from "@/components/Input/SelectBox"
+import {SelectBox} from "@/components/Input/SelectBox"
 import FormControl from '@mui/material/FormControl'
 import Rating from '@mui/material/Rating'
 import styled from "styled-components"
+import React from "react";
 
-export const SatisfiedCustomerDialog = React.FC = () => {
-  const [open, setOpen] = React.useState(false)
-  const [value, setValue] = React.useState<number | null>(2)
-  const [value2, setValue2] = React.useState<number | null>(2)
-  const options = [{ value: 'Casse', label: 'Unité cassé' },{ value: 'Incomplet', label: 'Livraison incompléte' },{ value: 'Retard', label: 'Livraison en retard' }];
-  const handleClickOpen = () => {
-    setOpen(true)
-  }
-  const handleClose = () => {
-    setOpen(false)
-  }
+export const SatisfiedCustomerDialog: React.FC = () => {
+    const [open, setOpen] = React.useState(false)
+    const [deliveryRatingNumber, setDeliveryRatingNumber] = React.useState<number | null>(10)
+    const [deliverymanRatingNumber, setDeliverymanRatingNumber] = React.useState<number | null>(10)
+    const options = [
+        {value: 'Casse', label: 'Unité cassé'},
+        {value: 'Incomplet', label: 'Livraison incompléte'},
+        {value: 'Retard', label: 'Livraison en retard'}
+    ];
 
-  return (
-    <React.Fragment >
-      <Container id="Client">
-        <div style={{
-          height: "100%",
-          margin: "3rem",
-          display: "flex",
-          flexDirection: "column",
-          gap: "1rem"
-        }}>
-          <Typography variant="h5" gutterBottom>Compte rendu de la livraison</Typography>
-          <IconButton
-            aria-label="close"
-            onClick={handleClose}
-            sx={{
-              position: 'absolute',
-              right: 8,
-              top: 8,
-              color: (theme) => theme.palette.grey[500],
-            }}
-          >
-            <CloseIcon/>
-            
-          </IconButton>
-          <FormControl>
-            <br/>
-            <Typography component="legend">Êtes-vous satisfait de votre livraison ?</Typography>
-            <Rating
-              name="simple-controlled"
-              value={value}
-              onChange={(event, newValue) => {
-                if (newValue >= 3) {
-                  // Correction du bug 5 etoiles sur 4
-                  if (newValue == 5) {newValue = 4}
-                  document.getElementById("ObservationBlock").classList.add("hidden")
-                }
-                // Si la note est inférieur a 3 on affiche le formulaire de mécontentement
-                else if(newValue < 3 && newValue != null) {
-                  document.getElementById("ObservationBlock").classList.remove("hidden")
-                }
+    const handleClickOpen = () => {
+        setOpen(true)
+    }
+    const handleClose = () => {
+        setOpen(false)
+    }
 
-                setValue(newValue);
-              }}
-            />
-          </FormControl>
-          <div id="ObservationBlock" class="hidden">
-            <SelectBox id="Observation1" label="Observation" options={options}/>
-            <br/> 
-            <SelectBox id="Observation2" label="Observation" options={options}/>
-          </div>
-          <FormControl>
-            <br/>
-            <Typography component="legend">Note du livreur</Typography>
-            <Rating
-              name="simple-controlled2"
-              value={value2}
-              onChange={(event, newValue2) => {
-                if (newValue2 == 5) {newValue2 = 4}
-                setValue2(newValue2);
-              }}
-            />
-          </FormControl>
-          
+    return (
+        <React.Fragment>
+            <Container id="Client">
+                <div style={{
+                    height: "100%",
+                    margin: "3rem",
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "1rem"
+                }}>
+                    <Typography variant="h5" gutterBottom>Compte rendu de la livraison</Typography>
+                    <IconButton
+                        aria-label="close"
+                        onClick={handleClose}
+                        sx={{
+                            position: 'absolute',
+                            right: 8,
+                            top: 8,
+                            color: (theme) => theme.palette.grey[500],
+                        }}
+                    >
+                        <CloseIcon/>
+                    </IconButton>
 
-          <br/>
-          <Button variant="contained" type="submit">Valider</Button>
-        </div>
-      </Container>
-    </React.Fragment>
-  )
+                    <FormControl>
+                        <br/>
+                        <Typography component="legend">Êtes-vous satisfait de votre livraison ?</Typography>
+                        <Rating
+                            name="delivery_rate"
+                            value={deliveryRatingNumber}
+                            max={10}
+                            size="large"
+                            onChange={(event, value) => setDeliveryRatingNumber(value)}
+                        />
+                    </FormControl>
+
+                    {((deliveryRatingNumber ?? 0) <= 5 ) && (
+                        <div id="ObservationBlock">
+                            <SelectBox id="Observation1" label="Observation 1" options={options}/>
+                            <br/>
+                            <SelectBox id="Observation2" label="Observation 2" options={options}/>
+                        </div>
+                    )}
+
+                    <FormControl>
+                        <br/>
+                        <Typography component="legend">Note du livreur</Typography>
+                        <Rating
+                            name="deliveryman_rate"
+                            value={deliverymanRatingNumber}
+                            max={10}
+                            size="large"
+                            onChange={(event, value) => setDeliverymanRatingNumber(value)}
+                        />
+                    </FormControl>
+
+
+                    <br/>
+                    <Button variant="contained" type="submit">Valider</Button>
+                </div>
+            </Container>
+        </React.Fragment>
+    )
 }
 
 export const Container = styled.div`
