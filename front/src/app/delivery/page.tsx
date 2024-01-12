7b748cf328e4129a79c0847919015d8b0339c3b6
Pellegrin69 <pellegrinpierre@hotmail.fr>
2024-01-12 11:43:15 +0100

    Correct bug on delivery page

--- front/src/app/delivery/page.tsx
+++ front/src/app/delivery/page.tsx
@@ -10,14 +10,18 @@ import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 function Delivery() {
-  const [orders, setOrders] = useState([])
+  const [delivery, setDelivery] = useState([])
 
   useEffect(() => {
-    Api.ajax("orders", "GET")
+    Api.ajax("deliveries/deliveryman/1578", "GET")
       .then(data => {
-        setOrders(data.content) // Mise à jour de l'état avec les nouvelles données
+        setDelivery(data.content)
       })
+
   }, [])
+
+  console.log(delivery.date)
+  console.log(delivery.orders)
 
   return (
     <Layout>
@@ -26,24 +30,24 @@ function Delivery() {
           <div className="client-info">
             <Chip label="Entreprise" sx={{ fontSize: "1.1rem", padding: "1rem" }} />
           </div>
-          <div className="state">
+          <div className="order-state">
             <Chip label="Statut" sx={{ fontSize: "1.1rem", padding: "1rem" }} />
           </div>
-          <div>
-            <Chip label="Adresse" sx={{ fontSize: "1.1rem", padding: "1rem" }} />
+          <div className="client-address">
+            <Chip label="Adresse" sx={{fontSize: "1.1rem", padding: "1rem"}}/>
           </div>
-          <div>
-            <Chip label="Date de livraison" sx={{ fontSize: "1.1rem", padding: "1rem" }} />
+          <div className="order-expected-time">
+            <Chip label="Date de livraison" sx={{fontSize: "1.1rem", padding: "1rem"}}/>
           </div>
-          <div>
-            <Chip label="Actions" sx={{ fontSize: "1.1rem", padding: "1rem" }} />
+          <div className="order-actions">
+            <Chip label="Actions" sx={{fontSize: "1.1rem", padding: "1rem"}}/>
           </div>
         </div>
         {
-          orders?.map(function (order) {
+          delivery?.orders?.map(function (order : any) {
             return <Customer data={{
               img_src: "/Theraputics.png",
-              name: "McDonalds",
+              phone: order.customer.phone,
               num: order?.id,
               state: order.state,
               address: `${order.customer.address} - ${order.customer.complement} - ${order.customer.zipcode}`,
--- front/src/components/Delivery/Customer.tsx
+++ front/src/components/Delivery/Customer.tsx
@@ -3,28 +3,35 @@
 import React from "react"
 
 function Customer(props: { data: any }) {
+    const formatDate = (dateString: string) => {
+        const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'short', year: 'numeric' };
+        const formattedDate = new Date(dateString).toLocaleDateString(undefined, options);
+
+        return formattedDate.replace(/\d+/, (match) => match);
+    };
+
     return (
-      <div className="client-delivery">
-        <div className="client-info">
-          <img src={`${props.data.img_src}`} alt=""/>
-          <div>
-            <p>{props.data.name}</p>
-            <p>N°{props.data.num}</p>
-          </div>
+        <div className="client-delivery">
+            <div className="client-info">
+                <img src={`${props.data.img_src}`} alt=""/>
+                <div>
+                    <p>{props.data.phone}</p>
+                    <p>N°{props.data.num}</p>
+                </div>
+            </div>
+            <div className="order-state">
+                <p>{props.data.state}</p>
+            </div>
+            <div className="client-address">
+                <p>{props.data.address}</p>
+            </div>
+            <div className="order-expected-time">
+                <p>{formatDate(props.data.date)}</p>
+            </div>
+            <div className="order-actions">
+                <p>{props.data.actions}</p>
+            </div>
         </div>
-        <div className="state">
-          <p>{props.data.state}</p>
-        </div>
-        <div>
-          <p>{props.data.address}</p>
-        </div>
-        <div>
-          <p>{props.data.date}</p>
-        </div>
-        <div>
-          <p>{props.data.actions}</p>
-        </div>
-      </div>
     )
 }
 
